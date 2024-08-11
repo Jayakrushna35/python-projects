0254e532551eeefb0a3855b640d18c54067e40f6
Jayakrushna35 <jayakrushnapattnaik6@gmail.com>
2024-08-11 17:17:16 +0530

    done

---
+++ Temperature Converter.py
@@ -0,0 +1,8 @@
+
+fahrenheit = int(input("What is the current Temperature :"))
+ 
+celsius = (fahrenheit-32)/1.8
+ 
+# printing the result
+print('%.2f Fahrenheit is equivalent to: %.2f Celsius'
+      % (fahrenheit ,celsius))
